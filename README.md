10f5a42dfb804fcdf5afd53ef87be1ea3d4baf24
Rodrigo <rmGuarachi@gmail.com>
2020-09-23 14:40:52 -0400

    modify README.md

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # crosscompute-cookiecutters
+pip install cookiecutter
+cookiecutter git@github.com:crosscompute/crosscompute-cookiecutters.git --directory="tool"
--- tool/hooks/pre_gen_project.py
+++ tool/hooks/pre_gen_project.py
@@ -4,7 +4,6 @@ import sys
 PACKAGE_PATTERN = re.compile(r'^[_a-zA-Z][_a-zA-Z0-9]+$')
 
 tool_name_slug = '{{ cookiecutter.tool_name_slug }}'
-print('&&&&&&', tool_name_slug)
 
 if not PACKAGE_PATTERN.match(tool_name_slug):
     sys.exit(f'tool_name: is not valid')
