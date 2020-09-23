b8d007b8c3fce578c553d68ac5b24de853c229c1
Rodrigo <rmGuarachi@gmail.com>
2020-09-23 14:30:23 -0400

    added tool cookiecutter

---
+++ tool/hooks/pre_gen_project.py
@@ -0,0 +1,10 @@
+import re
+import sys
+
+PACKAGE_PATTERN = re.compile(r'^[_a-zA-Z][_a-zA-Z0-9]+$')
+
+tool_name_slug = '{{ cookiecutter.tool_name_slug }}'
+print('&&&&&&', tool_name_slug)
+
+if not PACKAGE_PATTERN.match(tool_name_slug):
+    sys.exit(f'tool_name: is not valid')
